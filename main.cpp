da937091444e522418b1f6ba32fbd0f11d22fa8b
Ferdi Stoeltie <ferdistoeltie@gmail.com>
2016-04-07 15:25:17 +0200

    backup before failed event

--- main.cpp
+++ main.cpp
@@ -26,18 +26,31 @@ void findSubDirectoriesAndFiles(std::string dir, std::vector<std::string>& a)
 			cout << "Found File: " << FindFileData.cFileName << endl;
 			string test(FindFileData.cFileName);
 			std::string nextDir = dir + "/" + test;
-			
+			std::size_t endOfFileName;
 			if (!(test.find(".") != std::string::npos))	{
 				cout << "Found new subdirectory" << endl;
 				findSubDirectoriesAndFiles(nextDir, a);
 			}
-			else if (nextDir.find(".hpp") != std::string::npos)	{
-				cout << "Found hpp file" << endl;
-				a.push_back(nextDir);
+			else if ((endOfFileName = nextDir.find(".hpp")) != std::string::npos)	{
+				if(nextDir.length() == endOfFileName + 4)	{
+					cout << "endOfFileName " << endOfFileName << endl;
+					if(nextDir.find('.', endOfFileName + 2) == std::string::npos)	{
+						cout << "is .hpp only" << endl;
+					}
+					cout << "Found hpp file" << endl;
+					a.push_back(nextDir);
+				}
 			}
-			else if (nextDir.find(".cpp") != std::string::npos)	{
-				cout << "Found cpp file" << endl;
-				a.push_back(nextDir);
+			else if ((endOfFileName = nextDir.find(".cpp")) != std::string::npos)	{
+				cout << "endOfFileName " << endOfFileName << endl;
+				if(nextDir.length() == endOfFileName + 4)	{
+					char iOLE = nextDir.length() - 1; // index of last element
+					if(nextDir.find('.', endOfFileName + 2) == std::string::npos)	{
+						cout << "is .cpp only" << endl;
+					}
+					cout << "Found cpp file" << endl;
+					a.push_back(nextDir);
+				}
 			}
 		}
 		FindClose(hFind);
@@ -62,17 +75,87 @@ vector<string> getSubDirectories(string dir)	{
 	}
 	return files;
 }
-void changeDate(string& template_content)	{
+void changeDate(string& template_content, string& file)	{
+	// ToDo variables should be renamed...
 	std::size_t foundDate = template_content.find("<dd-mm-yy>");
-	time_t givenTime = time(0);
-	struct tm * now = localtime( & givenTime );
-	stringstream ss;
-	ss << "<" << now->tm_mday << "-" << (now->tm_mon + 1) << "-" << (now->tm_year + 1900 - 2000) << ">";
-	string currentDate;
-	ss >> currentDate;
-	template_content.replace(foundDate, 10, currentDate.c_str());
+	std::size_t foundDateField = file.find("@date");
+	if(foundDateField == string::npos){ // If file doesnt already contain @date field, add current date as date to file.
+		time_t givenTime = time(0);
+		struct tm * now = localtime( & givenTime );
+		stringstream ss;
+		string mDay;
+		if((int)(now->tm_mday / 10) >= 1)	{
+			stringstream ss0;
+			ss << ( now->tm_mday + 1 );
+			mDay = ss.str();
+		}
+		else	{
+			mDay = now->tm_mday + '0'; // + 48
+			mDay.insert(0, 1, '0');
+		}
+/*		stringstream ss12;
+		ss12 << 12;
+		mDay = ss12.str();
+		stringstream ss0;
+		ss0 << 12;
+		month = ss0.str();*/
+		string month;
+		if((int)((now->tm_mon + 1) / 10) == 1)	{
+			stringstream ss0;
+			ss << ( now->tm_mon + 1 );
+			month = ss.str();
+		}
+		else	{
+			month = (now->tm_mon + 1) + '0'; // + 48
+			month.insert(0, 1, '0');
+		}
+		cout << "month day is: " << mDay << endl;
+		ss << "<" << mDay << "-" << month << "-" << (now->tm_year + 1900 - 2000) << ">";
+		string currentDate;
+		ss >> currentDate;
+		template_content.replace(foundDate, 10, currentDate.c_str());
+		return;
+	}
+	while(file.at(foundDateField) < '0' || file.at(foundDateField) > '9')	{
+		cout << "file at: " << foundDateField << endl;
+		++foundDateField;
+	}
+	string dateField = "<";
+	
+	while(file.at(foundDateField) != '\r' && file.at(foundDateField) != '\n' )	{
+		cout << file.at(foundDateField);
+		dateField += file.at(foundDateField);
+		++foundDateField;
+	}
+	dateField += ">";
+	string emptyField("          ");
+	cout << "inserting date: " << dateField << endl;
+	template_content.replace(foundDate, 10, emptyField.c_str());
+	template_content.insert(foundDate, dateField.c_str());
+	
 }
-
+void changeVersion(string& template_content, string& file)	{
+	std::size_t foundVersion = template_content.find("<0.0.0>");
+	std::size_t foundVersionTag = file.find("@version");
+	if(foundVersionTag == string::npos){ // could not find version
+		return;
+	}
+	
+	while(file.at(foundVersionTag) < '0' || file.at(foundVersionTag) > '9')	{
+		cout << "file at: " << foundVersionTag << endl;
+		++foundVersionTag;
+	}
+	string versionField = "<";
+	while(file.at(foundVersionTag) != '\r' && file.at(foundVersionTag) != '\n' )	{
+		cout << file.at(foundVersionTag);
+		versionField += file.at(foundVersionTag);
+		++foundVersionTag;
+	}
+	versionField +=  ">";
+	string emptyField("       ");
+	template_content.replace(foundVersion, 10, emptyField.c_str());
+	template_content.insert(foundVersion, versionField.c_str());
+}
 void changeAuthor(string& template_content, string& file)	{
 	std::size_t foundAuthorField = template_content.find("<full name>");
 	if(foundAuthorField == string::npos){
@@ -90,7 +173,7 @@ void changeAuthor(string& template_content, string& file)	{
 	string authorName("<");
 	uint8_t authorNameLength = 0;
 	// get name
-	while(file.at(foundAuthorTag) >= 'A' && file.at(foundAuthorTag) <= 'z')	{
+	while(file.at(foundAuthorTag) != '\r' && file.at(foundAuthorTag) != '\n' )	{
 		cout << file.at(foundAuthorTag);
 		authorName += file.at(foundAuthorTag);
 		++authorNameLength;
@@ -98,9 +181,57 @@ void changeAuthor(string& template_content, string& file)	{
 	}
 	cout << endl;
 	authorName += ">";
-	template_content.replace(foundAuthorField, authorNameLength, authorName);
-		
+	cout << "author name is: " << authorName << endl;
+	string emptyField("           ");
+	template_content.replace(foundAuthorField, 11, emptyField.c_str());
+	template_content.insert(foundAuthorField, authorName.c_str());
+	//template_content.replace(foundAuthorField, authorNameLength, authorName.c_str());
 }
+void removeTemplate(string& file)	{
+	string t_identifier("++--++");
+	std::size_t eot = file.find_last_of(t_identifier); // end of template
+	file.erase(0, eot + t_identifier.length());
+	cout << "Removed old template version..." << endl;
+}
+void validateTemplateVersion(string& template_content, string& file)	{
+	string version_identifier("#version");
+	std::size_t file_template_tag = file.find(version_identifier);
+	if(file_template_tag == std::string::npos)	{ // if file does not contain version tag
+		cout << "file does not contain template version!" << endl;
+		return;
+	}
+	// get template in file version and store in int
+	string r_file_content = file;
+	r_file_content.erase(0, file_template_tag + version_identifier.length());
+	stringstream ss1(r_file_content);
+	int file_version;
+	ss1 >> file_version;
+	
+	std::size_t template_version_tag = template_content.find(version_identifier);
+	if(template_version_tag == std::string::npos)	{	// if template does not contain version tag
+		cout << "template does not contain template version!" << endl;
+		return;
+	}
+	
+	string t_template_content = template_content;
+	t_template_content.erase(0, file_template_tag + version_identifier.length());
+	stringstream ss2(t_template_content);
+	int template_version;
+	ss2 >> template_version;
+	
+	if(template_version > file_version)
+		removeTemplate(file);
+}
+void changeFileName(string& template_content, string fileName)	{
+	std::size_t foundFile = template_content.find("<filename>");
+	std::size_t foundName = fileName.find_last_of('/');
+	string fName = "<" + fileName.substr(foundName + 1, fileName.length()) + ">";
+	string emptyField("          ");
+	template_content.replace(foundFile, 10, emptyField.c_str());
+	template_content.insert(foundFile, fName);
+	cout << "replaced string filename with " << fName << endl;
+}
+
 int main(int argc, char* argv[])
 {
 	string t_file;
@@ -117,20 +248,23 @@ int main(int argc, char* argv[])
                        (std::istreambuf_iterator<char>()    ) );
 	ifs.close();
 	cout << template_content;
-	changeDate(template_content);
 	
 	
 	for (string& str : getSubDirectories("../"))	{
-		cout << "Writing into file..." << endl;
+		string filled_template = template_content;
+		cout << "Writing into file... " << str << endl;
 		ofstream oFile(str + "format");
 		ifstream iFile(str);
 		std::string hpp_content( (std::istreambuf_iterator<char>(iFile) ),
 								(std::istreambuf_iterator<char>()    ) );
-		changeAuthor(template_content, hpp_content);
-
+		validateTemplateVersion(filled_template, hpp_content);
+		changeAuthor(filled_template, hpp_content);
+		changeDate(filled_template, hpp_content);
+		changeVersion(filled_template, hpp_content);
+		changeFileName(filled_template, str);
 		std::size_t found = hpp_content.find("++--++");
 		if(found == string::npos)	{
-			oFile << template_content << endl; // write template
+			oFile << filled_template << endl; // write template
 			oFile << hpp_content;
 			iFile.close();
 			oFile.close();
@@ -150,9 +284,7 @@ int main(int argc, char* argv[])
 			  else
 				puts( "File successfully deleted" );
 		}
-		
 	}
-
 	// Read template file
 	return 0;
 }
